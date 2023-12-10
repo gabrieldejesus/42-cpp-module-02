a0becfc34e1154ad389b4c7a6586954df24c589f
Gabriel de Jesus <devgabrieldejesus@gmail.com>
2023-12-10 15:15:56 -0300

    feat: initial structure to exercise 00

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gde-jesu <gde-jesu@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/10 15:13:26 by gde-jesu          #+#    #+#             */
+/*   Updated: 2023/12/10 15:15:06 by gde-jesu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "Fixed.hpp"
+
+Fixed::Fixed(void)
+{
+ 	std::cout << "Constructor called!" << std::endl;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called!" << std::endl;
+}
